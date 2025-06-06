ab95fe4fb1facd7eb157e6c33045ca9ff57b392a
Developer-Pratham <prathamvajpayee2435@gmail.com>
2025-06-06 05:57:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ const game = {}
 app.use(express.json())
 app.use(express.urlencoded({ extended:true }))
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('app');
